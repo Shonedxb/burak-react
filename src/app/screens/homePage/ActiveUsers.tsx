38fd165ea88ecc8128f032da9e5055d663336daa
“Shonedxb” <firdavs.cra@gmail.com>
2025-05-19 02:58:53 +0400

    fix: retrieve top users and modify active users component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -3,14 +3,22 @@ import Card from "@mui/joy/Card";
 import { CssVarsProvider, Typography } from "@mui/joy";
 import AspectRatio from "@mui/joy/AspectRatio";
 
-const activeUsers = [
-    { memberNick: "Martin", memberImage: "/img/martin.webp" },
-    { memberNick: "Justin", memberImage: "/img/justin.webp" },
-    { memberNick: "Rose", memberImage: "/img/rose.webp" },
-    { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
-];
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveTopUsers } from './selector';
+import { serverApi } from "../../../lib/config";
+import { Member } from "../../../lib/types/member";
+
+
+//** REDUX SLICE & SELECTOR **/
+const topUsersRetriever = createSelector(
+  retrieveTopUsers, (topUsers) => ({ topUsers
+    
+   }));
+
 
 export default function ActiveUsers() {
+    const { topUsers } = useSelector(topUsersRetriever);
     return (
         <div className={"active-users-frame"}>
             <Container>
@@ -18,17 +26,20 @@ export default function ActiveUsers() {
                     <Box className={"category-title"}>Active Users</Box>
                     <Stack className={"cards-frame"}>
                         <CssVarsProvider>
-                            {activeUsers.length !== 0 ? (
-                                activeUsers.map((ele, index) => (
-                                    <Card key={index} className={"card"}>
+                            {topUsers.length !== 0 ? (
+                                topUsers.map((member: Member) => {
+                                    const imagePath = `${serverApi}/${member.memberImage}`;
+                                    return (
+                                    <Card key={member._id} className={"card"}>
                                         <AspectRatio ratio="1">
-                                            <img src={ele.memberImage} alt={ele.memberNick} />
+                                            <img src={imagePath} alt="" /> 
                                         </AspectRatio>
                                         <Typography className={"member-nickname"}>
-                                            {ele.memberNick}
+                                            {member.memberNick}
                                         </Typography>
                                     </Card>
-                                ))
+                                    );
+                                })
                             ) : (
                                 <Box className="no-data">No Active Users!</Box>
                             )}
